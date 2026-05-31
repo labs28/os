be3ac3599818613d3d909fda645ed3ecfa817920
labs28 <darksoulimmortal@gmail.com>
2026-05-31 22:32:49 +0530

    Create readanother.c

---
+++ readanother.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    FILE *f1 = fopen("a.txt", "r");
+    FILE *f2 = fopen("b.txt", "w");
+
+    char ch;
+
+    while((ch=fgetc(f1))!=EOF)
+        fputc(ch,f2);
+
+    fclose(f1);
+    fclose(f2);
+
+    return 0;
+}
